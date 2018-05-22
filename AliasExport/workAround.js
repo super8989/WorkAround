9b02fcebd505af6c10a3abe1ae4d58605d768339
Sam Yang <super8989@gmail.com>
2018-05-22 16:09:54 -0400

    alias export

---
+++ AliasExport/workAround.js
@@ -0,0 +1,17 @@
+//import the module using aliases
+import {Employee, cadre, tax, benefits, bonus, reimbursement} from './employee';
+
+
+// modify the variables such that each of the variables is written as its alias
+function getEmployeeInformation(inputSalary) {
+  Employee.salary = inputSalary;
+  console.log('Cadre: ' + cadre());
+  console.log('Tax: ' + tax());
+  console.log('Benefits: ' + benefits());
+  console.log('Bonus: ' + bonus());
+  console.log('Reimbursement Eligibility: ' + reimbursement() + '\n');
+}
+
+getEmployeeInformation(10000);
+getEmployeeInformation(50000);
+getEmployeeInformation(100000);
