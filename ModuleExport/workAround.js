ddf67e6fe58652d74aaf9e0596af8a2ab80c6d77
Sam Yang <super8989@gmail.com>
2018-05-22 16:10:27 -0400

    module export

---
+++ ModuleExport/workAround.js
@@ -0,0 +1,17 @@
+//import the module using ./employee file path
+import Employee from './employee';
+
+
+// modify the variables such that they are a property on the Employee module.
+function getEmployeeInformation(inputSalary) {
+  Employee.salary = inputSalary;
+  console.log('Cadre: ' + Employee.getCadre());
+  console.log('Tax: ' + Employee.calculateTax());
+  console.log('Benefits: ' + Employee.getBenefits());
+  console.log('Bonus: ' + Employee.calculateBonus());
+  console.log('Reimbursement Eligibility: ' + Employee.reimbursementEligibility() + '\n');
+}
+
+getEmployeeInformation(10000);
+getEmployeeInformation(50000);
+getEmployeeInformation(100000);
